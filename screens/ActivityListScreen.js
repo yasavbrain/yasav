5d04174f4547ac96d2242f45e1761f3384c6f2b6
Antoine <antoine@nebulae.co>
2017-10-31 20:49:39 +0100

    Base of list & add screens

--- screens/ActivityListScreen.js
+++ screens/ActivityListScreen.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { Content, Button, Text } from 'native-base';
+import { View, ListView } from 'react-native';
+import ActivityAdd from './ActivityAddScreen';
 
 export default class ActivityListScreen extends React.Component {
 
-  render() {
+    constructor(props){
+        super(props);
 
-    const { navigate } = this.props.navigation;
-    return(
-      <Content>
-        <Button primary onPress={() => navigate('ActivityViewScreen')}>
-          <Text>Voir une activité</Text>
-        </Button>
-        <Text>En tant quutilisateur, je veux pouvoir visualiser la liste de
-          mes activités</Text>
-      </Content>
-    );
-  }
+        this.state = {
+            activityList: [ { title: "lol", content: "pascontent"} ],
+            adding: false
+        }
+        this.addElementToList = this.addElementToList.bind(this);
+
+
+    }
+
+    addElementToList (e) {
+        let activities = this.state.activityList;
+        activities.push(e)
+       
+        this.setState( { activityList: activities, adding: false });
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        if(this.state.adding){
+            return <ActivityAdd callback={this.addElementToList}/>
+        }else{
+            return(
+                <View>
+                    <ListView dataSource={ds.cloneWithRows(this.state.activityList)} 
+                renderRow={(row, j, k) => <Text node={row} index={parseInt(k,10)}>{row.title} </Text> } />
+                    <Button primary onPress={() => navigate('ActivityViewScreen')}>
+                            <Text>Voir une activité</Text>
+                    </Button>
+
+                    <Button full primary onPress={() => this.setState({adding: true})}><Text>Ajouter une activité</Text></Button>
+                </View>
+            );
+        }
+
+    }
 }
 
  ActivityListScreen.navigationOptions = () => ({
-   title: 'Liste des activités'
+   title: 'Activités'
 })
