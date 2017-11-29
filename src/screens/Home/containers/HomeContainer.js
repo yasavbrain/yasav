fef1e615e2e2b91a4dbfdf9116dd20251e6963da
Antoine <antoine@nebulae.co>
2017-11-29 17:02:15 +0100

    And home now

---
+++ src/screens/Home/containers/HomeContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeView from '../views/HomeView'
+
+export default class HomeContainer extends React.Component {
+
+  render() {
+    return(
+      <HomeView
+        navigateToActivityList={this.props.navigateToActivityList}
+        navigateToTodoList={this.props.navigateToTodoList}
+        navigateToActivityAdd={this.props.navigateToActivityAdd}
+        />
+    );
+  }
+}
