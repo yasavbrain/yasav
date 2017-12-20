9ff4dd7cebbab5d81947af0ea23a65d65ee1e018
Antoine <antoine@nebulae.co>
2017-12-20 10:20:42 +0100

    Moving from React-native to native-base

--- src/components/Home/views/HomeView.js
+++ src/components/Home/views/HomeView.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Content, Button, Text } from 'native-base';
-import I18n from 'yasav/locales/i18n'
+import I18n from 'yasav/locales/i18n';
 
 export default class HomeView extends React.Component {
 
   render() {
     return (
       <Content>
-        <Button primary style={{marginTop: 10}} onPress={() => this.props.navigateToActivityList()}>
+        <Button full primary style={{marginTop: 10}} onPress={this.props.navigateToActivityList}>
           <Text>{I18n.t('home.activityListButton')}</Text>
         </Button>
 
-        <Button primary style={{marginTop: 10}} onPress={() => this.props.navigateToTodoList()}>
+        <Button full primary style={{marginTop: 10, height: 150}} onPress={this.props.navigateToTodoList}>
           <Text>{I18n.t('home.todoListButton')}</Text>
         </Button>
 
-        <Button primary style={{marginTop: 10}} onPress={() => this.props.navigateToActivityAdd()}>
+        <Button full primary style={{marginTop: 10}} onPress={this.props.navigateToActivityAdd}>
           <Text>{I18n.t('home.addActivityButton')}</Text>
         </Button>
       </Content>
--- src/components/activity/screens/ActivityList/views/ActivityListView.js
+++ src/components/activity/screens/ActivityList/views/ActivityListView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Content, Button, Text } from 'native-base';
+import { Content, Button, Text, List, ListItem } from 'native-base';
 import { FlatList } from 'react-native';
 import I18n from 'yasav/locales/i18n'
 
@@ -9,18 +9,17 @@ export default class ActivityListView extends React.Component {
   render() {
     return(
       <Content>
-        <FlatList
-          data={this.props.displayActivityList}
-          renderItem={(row, i) => {
-            return <Text 
-              style={{height: 75, fontSize: 50}}
-              onPress={ () => this.props.navigateToActivityDisplayScreen(row.item) }
-            > 
-              {row.item.title}
-            </Text>
-            }
-          }
-        />
+        <List dataArray={this.props.displayActivityList}
+            renderRow={(item) =>
+              <ListItem
+                style={{height: 75, backgroundColor: 0}}
+                onPress={ () => this.props.navigateToActivityDisplayScreen(item) }
+              >
+                <Text style={{fontSize: 50}}>{item.title}</Text>
+              </ListItem>
+            }>
+        </List>
+
         <Button full primary onPress={() => {this.props.navigateToActivityAddScreen()}}>
           <Text>{I18n.t('activity.activityList.addActivityButton')}</Text>
         </Button>
