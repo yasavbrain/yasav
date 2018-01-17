d804cfa23597884d1b4ffb2bb1e238979aa6d48d
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-01-17 09:46:09 +0100

    Adding a package and trying to to somme DB calls in action creators.

--- src/components/todo/screens/TodoAdd/actions/index.js
+++ src/components/todo/screens/TodoAdd/actions/index.js
@@ -1,9 +1,23 @@
 import { ADD_TODO, TOGGLE_TODO } from './types';
+import db, { executeSql } from 'yasav/src/Database';
 
-export function addTodo(todo) {
-  return { type: ADD_TODO, todo: todo }
-}
+
+// export function addTodo(todo) {
+//   return { type: ADD_TODO, todo: todo }
+// }
 
 export function toggleTodo(id) {
   return { type: TOGGLE_TODO, todo_id : id }
+}
+
+export function addTodo(todo) {
+  return (dispatch, getState) => {
+
+    executeSql(`INSERT INTO todo (title, status) values (?, ?)`, [todo.title, todo.completed])
+    .then(res => {
+      dispatch({ type: ADD_TODO, todo: todo });
+      executeSql(`SELECT * from todo`)
+      .then(res => console.log(res))
+    })
+  };
 }
