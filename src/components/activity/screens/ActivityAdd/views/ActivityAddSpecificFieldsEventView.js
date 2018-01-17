0db4543986edcb6cc2cfb9cd3a9ca744d5eaa736
Antoine <antoine@nebulae.co>
2018-01-17 09:20:50 +0100

    Removing What field for event - redoundancy with title

--- src/components/activity/screens/ActivityAdd/views/ActivityAddSpecificFieldsEventView.js
+++ src/components/activity/screens/ActivityAdd/views/ActivityAddSpecificFieldsEventView.js
@@ -7,12 +7,12 @@ import Style from '../styles/style.js';
 export default class ActivityAddSpecificFieldsEventView extends React.Component {
 
   render() {
-    console.log(this.props.setEventWhat)
-    return(
+    return null;
+    /*return(
       <Item floatingLabel>
         <Label>{I18n.t('activity.activityAdd.eventWhat')}</Label>
         <Input onChangeText={this.props.setEventWhat} value={this.props.value} />
       </Item>
-    );
+    );*/
   }
 }
