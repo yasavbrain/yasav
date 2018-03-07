04c2bd2a3797fa094529c50e2bdbfe8189e92817
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-03-07 15:18:40 +0100

    Adding ART Support.

--- src/components/graph/screens/GraphTagDisplay/views/GraphTagDisplayView.js
+++ src/components/graph/screens/GraphTagDisplay/views/GraphTagDisplayView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Content, Text } from 'native-base';
 import I18n from 'yasav/locales/i18n'
 import { GenericHeader } from 'yasav/src/viewElements/shared/Header';
+import { ART } from 'react-native';
 
 export default class GraphTagDisplayView extends React.Component {
   render() {
@@ -12,7 +13,15 @@ export default class GraphTagDisplayView extends React.Component {
           title={I18n.t('activity.activityDisplay.title')}
         />
         <Content>
-          <Text>Loooool</Text>
+          <ART.Surface width={1000} height={600}>
+            <ART.Group>
+              <ART.Shape
+                d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+                stroke="#000"
+                strokeWidth={1}
+              />
+            </ART.Group>
+          </ART.Surface>
         </Content>
       </Container>
     );
