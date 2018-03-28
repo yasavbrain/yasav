0d57186ef3e0d7254e1596a83da8c3b317283c10
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-03-28 16:46:02 +0200

    Working version of pan & zoom with touch & pinch !!

--- src/components/graph/screens/GraphTagDisplay/views/GraphTagDisplayView.js
+++ src/components/graph/screens/GraphTagDisplay/views/GraphTagDisplayView.js
@@ -12,91 +12,131 @@ const d3 = {
   scale,
 }
 
+// based on https://snack.expo.io/@msand/svg-pinch-to-pan-and-zoom
+function calcDistance(x1, y1, x2, y2) {
+  const dx = x1 - x2;
+  const dy = y1 - y2;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+function middle(p1, p2) {
+  return (p1 + p2) / 2;
+}
+
+function calcCenter(x1, y1, x2, y2) {
+  return {
+    x: middle(x1, x2),
+    y: middle(y1, y2),
+  };
+}
+
+
 export default class GraphTagDisplayView extends React.Component {
   constructor(props) {
     super(props);
-    mainScaleX = d3.scale.scaleLinear()
-        .domain([0, 400])
-        .range([0, 400]);
-    mainScaleY = d3.scale.scaleLinear()
-      .domain([0, 400])
-      .range([0, 400]);
     this.state = {
-      x: 0,
-      y: 0,
-      mainScaleX: mainScaleX,
-      mainScaleY: mainScaleY,
-      scale: 1,
-      rescaledX: d3.zoom.zoomIdentity.rescaleX(mainScaleX),
-      rescaledY: d3.zoom.zoomIdentity.rescaleY(mainScaleY),
-    }
-    this.touch = {
-      x: null,
-      y: null,
+      zoom: 1,
+      left: 0,
+      top: 0,
+    };
+  }
+
+  processPinch(x1, y1, x2, y2) {
+    const distance = calcDistance(x1, y1, x2, y2);
+    const { x, y } = calcCenter(x1, y1, x2, y2);
+
+    if (!this.state.isZooming) {
+      const { top, left, zoom } = this.state;
+      this.setState({
+        isZooming: true,
+        initialX: x,
+        initialY: y,
+        initialTop: top,
+        initialLeft: left,
+        initialZoom: zoom,
+        initialDistance: distance,
+      });
+    } else {
+      const {
+        initialX,
+        initialY,
+        initialTop,
+        initialLeft,
+        initialZoom,
+        initialDistance,
+      } = this.state;
+
+      const touchZoom = distance / initialDistance;
+      const dx = x - initialX;
+      const dy = y - initialY;
+
+      const left = (initialLeft + dx - x) * touchZoom + x;
+      const top = (initialTop + dy - y) * touchZoom + y;
+      const zoom = initialZoom * touchZoom;
+
+      this.setState({
+        zoom,
+        left,
+        top,
+      });
     }
   }
+
+  processTouch(x, y) {
+    if (!this.state.isMoving || this.state.isZooming) {
+      const { top, left } = this.state;
+      this.setState({
+        isMoving: true,
+        isZooming: false,
+        initialLeft: left,
+        initialTop: top,
+        initialX: x,
+        initialY: y,
+      });
+    } else {
+      const { initialX, initialY, initialLeft, initialTop } = this.state;
+      const dx = x - initialX;
+      const dy = y - initialY;
+      this.setState({
+        left: initialLeft + dx,
+        top: initialTop + dy,
+      });
+    }
+  }
+
   componentWillMount() {
     this._panResponder = PanResponder.create({
-      onMoveShouldSetResponderCapture: () => true, //Tell iOS that we are allowing the movement
-      onMoveShouldSetPanResponderCapture: () => true, // Same here, tell iOS that we allow dragging
-      onMoveShouldSetPanResponder: (evt, gestureState) => true,
-      onPanResponderGrant: (evt, { x0, y0 }) => {
-        console.log("touch")
-        this.touch = {
-          x: x0,
-          y: y0
-        };
+      onPanResponderGrant: () => {},
+      onPanResponderTerminate: () => {},
+      onMoveShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponder: () => true,
+      onShouldBlockNativeResponder: () => true,
+      onPanResponderTerminationRequest: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onStartShouldSetPanResponderCapture: () => true,
+      onPanResponderMove: evt => {
+        const touches = evt.nativeEvent.touches;
+        const length = touches.length;
+        if (length === 1) {
+          const [{ locationX, locationY }] = touches;
+          this.processTouch(locationX, locationY);
+        } else if (length === 2) {
+          const [touch1, touch2] = touches;
+          this.processPinch(
+            touch1.locationX,
+            touch1.locationY,
+            touch2.locationX,
+            touch2.locationY
+          );
+        }
       },
-      onPanResponderMove: (event, {stateID, vx, vy, moveX, moveY, pinch, numberActiveTouches}) => {
-        xDiff = moveX - this.touch.x
-        yDiff = moveY - this.touch.y
-        // console.log("MOVE", xDiff, yDiff)
-        this.touch = {
-          x: moveX,
-          y: moveY,
-        }
-        this.setState({ x: this.state.x + xDiff, y: this.state.y + yDiff});
-        console.log(event.nativeEvent.touches)
-
-        if (numberActiveTouches >= 2) {
-          let touch1 = event.nativeEvent.touches[0];
-          let touch2 = event.nativeEvent.touches[1];
-          console.log(Math.ceil(yDiff) / this.props.height)
-          this.setState({ scale: this.state.scale - Math.ceil(yDiff) / this.props.height})
-          // const direction = yDiff > 0 ? 'out' : 'in';
-          // if (direction === 'in') {
-          //   this.setState({ scale: this.state.scale + yDiff / })
-          // } else {
-          //   this.setState({ scale: this.state.scale - 0.05})
-          // }
-        }
-
-
-        // console.log("onPanResponderMove", dx, dy)
-        // if (numberActiveTouches >= 2) {
-        //   const scaleX = this.state.scale - (dx / this.props.height) * this.state.scale
-        //   const scaleY = this.state.scale - (dy / this.props.width) * this.state.scale
-        //   max = Math.min(scaleX, scaleY)
-        // } else {
-        //   max = this.state.scale
-        // }
-        //
-        // console.log("max", max)
-        // const zoomIdentity = d3.zoom.zoomIdentity.translate(dx, dy).scale(1)
-        // this._onDrag(zoomIdentity, max)
+      onPanResponderRelease: () => {
+        this.setState({
+          isZooming: false,
+          isMoving: false,
+        });
       },
     });
-  }
-
-  _onDrag(zoomIdentity, newScale){
-    console.log("DRAGGING", zoomIdentity)
-    const rescaledX = zoomIdentity.rescaleX(this.state.mainScaleX)
-    const rescaledY = zoomIdentity.rescaleY(this.state.mainScaleY)
-    this.setState({
-      rescaledX: zoomIdentity.rescaleX(this.state.mainScaleX),
-      rescaledY: zoomIdentity.rescaleY(this.state.mainScaleY),
-      scale: newScale,
-    })
   }
 
   render() {
@@ -108,7 +148,7 @@ export default class GraphTagDisplayView extends React.Component {
         />
         <View {...this._panResponder.panHandlers}>
           <ART.Surface width={this.props.width} height={this.props.height}>
-            <ART.Group x={this.state.x} y={this.state.y} scaleX={this.state.scale} scaleY={this.state.scale}>
+            <ART.Group x={this.state.left} y={this.state.top} scaleX={this.state.zoom} scaleY={this.state.zoom}>
               {
                 // this.props.nodes.map(item => (
                 //   <ARTNode
