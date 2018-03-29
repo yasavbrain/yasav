fd76ac7907b14336c3254eb5f95d0a853ec2b6e5
Alexandre Sioufi <sioufia78@gmail.com>
2018-03-29 12:14:31 +0200

    Added the split request of each term for activity

--- src/components/interlocutor/screens/InterlocutorList/actions/index.js
+++ src/components/interlocutor/screens/InterlocutorList/actions/index.js
@@ -17,27 +17,40 @@ export function getInterlocutorList() {
 }
 
 export function getInterlocutorListFromRequest(request) {
-  if ( request.length > 0 ) {
-    return (dispatch, getState) => executeSql(
-      `SELECT name, link_to_me, id 
+  if (request.length > 0) {
+    requestList = request.split(' ');
+    return (dispatch, getState) => {
+      let promises = [];
+      request = `SELECT name, link_to_me, id 
       FROM interlocutor
-      WHERE name LIKE ? OR link_to_me LIKE ?`,
-      ["%" + request + "%", "%" + request + "%"]
-    )
-      .then((res) => {
-        const interlocutorListFromRequest = res.rows._array.map(interlocutor => ({
-          interlocutor: {
-            name: interlocutor.name,
-            link_to_me: interlocutor.link_to_me,
-            id: interlocutor.id,
-          }
-        }));
-        dispatch({ type: GET_INTERLOCUTOR_LIST_FROM_REQUEST, interlocutorListFromRequest });
-        return res
+      WHERE name LIKE ? OR link_to_me LIKE ?`;
+      requestList.forEach((term) => {
+        promises = promises.concat(executeSql(request, ['%' + term + '%', '%' + term + '%']));
       });
+      return Promise.all(promises)
+        .then((res) => {
+          let interlocutorListFromRequest = [];
+          let ids = [];
+          res.forEach((r) => {
+            r.rows._array.forEach((interlocutor) => {
+              if (ids.indexOf(interlocutor.id) == -1) {
+                interlocutorListFromRequest = interlocutorListFromRequest.concat({
+                  interlocutor: {
+                    name: interlocutor.name,
+                    link_to_me: interlocutor.link_to_me,
+                    id: interlocutor.id,
+                  },
+                });
+                ids = ids.concat(interlocutor.id);
+              }
+            });
+          });
+          dispatch({ type: GET_INTERLOCUTOR_LIST_FROM_REQUEST, interlocutorListFromRequest });
+        });
+    };
   }
   else {
-    interlocutorListFromRequest = []
-    return (dispatch) => dispatch({ type: GET_INTERLOCUTOR_LIST_FROM_REQUEST, interlocutorListFromRequest });
+    interlocutorListFromRequest = [];
+    return dispatch => dispatch({ type: GET_INTERLOCUTOR_LIST_FROM_REQUEST, interlocutorListFromRequest });
   }
 }
